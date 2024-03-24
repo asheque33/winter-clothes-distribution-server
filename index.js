4f0f5477b8bc257d807e7a22a10a47fc555a0e68
asheque33 <ashequrrahman3333@gmail.com>
2024-03-24 08:30:59 +0600

    winter clothes crud operations completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
@@ -9,7 +9,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
+// app.use(cors());
 app.use(express.json());
 
 // MongoDB Connection URL
@@ -25,15 +26,16 @@ async function run() {
     await client.connect();
     console.log("Connected to MongoDB");
 
-    const db = client.db("assignment");
-    const collection = db.collection("users");
+    const db = client.db("wc-project");
+    const userCollection = db.collection("users");
+    const winterClothesCollection = db.collection("winter-clothes");
 
     // User Registration
-    app.post("/api/v1/register", async (req, res) => {
+    app.post("/api/auth/register", async (req, res) => {
       const { name, email, password } = req.body;
 
       // Check if email already exists
-      const existingUser = await collection.findOne({ email });
+      const existingUser = await userCollection.findOne({ email });
       if (existingUser) {
         return res.status(400).json({
           success: false,
@@ -45,7 +47,7 @@ async function run() {
       const hashedPassword = await bcrypt.hash(password, 10);
 
       // Insert user into the database
-      await collection.insertOne({ name, email, password: hashedPassword });
+      await userCollection.insertOne({ name, email, password: hashedPassword });
 
       res.status(201).json({
         success: true,
@@ -54,11 +56,11 @@ async function run() {
     });
 
     // User Login
-    app.post("/api/v1/login", async (req, res) => {
+    app.post("/api/auth/login", async (req, res) => {
       const { email, password } = req.body;
 
       // Find user by email
-      const user = await collection.findOne({ email });
+      const user = await userCollection.findOne({ email });
       if (!user) {
         return res.status(401).json({ message: "Invalid email or password" });
       }
@@ -82,7 +84,78 @@ async function run() {
     });
 
     // ==============================================================
-    // WRITE YOUR CODE HERE
+    app.post("/dashboard/create-winter-clothes", async (req, res) => {
+      const { image, category, title, size, description } = req.body;
+      const result = await winterClothesCollection.insertOne({
+        image,
+        category,
+        title,
+        size,
+        description,
+      });
+      if (result) {
+        res.status(201).json({
+          success: true,
+          message: "Winter CLothes created successfully",
+          data: req.body,
+        });
+      }
+    });
+    // get Winter Clothes collection from 'winter-clothes' server collection
+    app.get("/winter-clothes", async (req, res) => {
+      const result = await winterClothesCollection.find().toArray();
+      res.status(200).json({
+        success: true,
+        message: "Winter clothes retrieved successfully",
+        data: result,
+      });
+    });
+    app.get("/winter-clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await winterClothesCollection.findOne(query);
+      res.status(200).json({
+        success: true,
+        message: "Winter cloth retrieved successfully",
+        data: result,
+      });
+    });
+    app.patch("/winter-clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedCloth = {
+        $set: {
+          image: data.image,
+          title: data.title,
+          category: data.category,
+          size: data.size,
+          description: data.description,
+        },
+      };
+      const result = await winterClothesCollection.updateOne(
+        query,
+        updatedCloth,
+        options
+      );
+      res.status(200).json({
+        success: true,
+        message: "Winter cloth updated successfully",
+        data: result,
+      });
+    });
+    app.delete("/winter-clothes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await winterClothesCollection.deleteOne(query);
+      res.status(200).json({
+        success: true,
+        message: "Winter cloth deleted successfully",
+        data: result,
+      });
+    });
+
     // ==============================================================
 
     // Start the server
